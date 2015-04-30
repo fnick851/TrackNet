804c435d3b51ad2202cec2df21e1ff9416f7da1f
Beth McNany <beth.mcnany@gmail.com>
2015-04-30 01:00:28 -0400

    get categories from actual data and sort on them; load jsons in order

--- WebContent/blocks/autoblocks.js
+++ WebContent/blocks/autoblocks.js
@@ -15,47 +15,22 @@ var firstparty, thirdparty;
 var colors = d3.scale.category10();
 var selectedBlock = null;
 
-// todo load automatically
-var active_categories = [
-	{ value: 'Health', data: '30%', enabled: false },
-	{ value: 'Shopping', data: '19%', enabled: false },
-	{ value: 'Search Engines/Portals', data: '12%', enabled: false },
-	{ value: 'Vehicles', data: '8%', enabled: false },
-];
-
-var other_categories = [
-	{ value: 'Financial Services', data: '15%', enabled: false },
-	{ value: 'Technology/Internet', data: '12%', enabled: false },
-	{ value: 'Society/Daily Living', data: '12%', enabled: false },
-	{ value: 'News/Media', data: '8%', enabled: false },
-	{ value: 'Uncategorized', data: '5%', enabled: false },
-];
+var active_categories;
+var other_categories;
 
 function runMain() {
-	// load autocomplete box
-	//TODO:
-	//could be changed to get the top 5 categories from other_categories lists, move them to active_categories
-	for(var i = 0; i < active_categories.length; i++)
-	{
-		var html = '<div class="active_category_item">' +
-			'<button class="delete_item" onclick="remove_active_item(this);"></button>' +
-			'<span class="item_name">' + active_categories[i].value + '</span> <span class="separator">|</span> ' +
-			'<i><span class="item_percent">' + active_categories[i].data + '</span></i>' +
-			'<input type="checkbox">' +
-			'</div>';
-
-		$('#active_categories').append(html);
-	}
-
+	// todo: occasionally categories don't load until after the data which causes problems
 	d3.json("domainCategoryDict.json", function(error, json) {
 		if (error) return alert("Error loading categories: " + error);
 		categories = json;
-	});
-	
-	d3.json("257.data.json", function(error, json) {
-	  if (error) return alert("Error loading data: " + error);
-	  td = json;
-	  visualizeit();
+		
+		d3.json("257.data.json", function(error, json) {
+		if (error) return alert("Error loading data: " + error);
+			td = json;
+			initializeCategories();
+			initializeSearchBox();
+			visualizeit();		  
+		});
 	});
 }
 
@@ -109,6 +84,87 @@ function addCategoriesToJson(datalist) {
 	return datalist;
 }
 
+function initializeCategories() {
+	active_categories = [];
+	other_categories = [];
+	clist = {};
+	// todo switch to datalist so we get only categories for this data set
+	firstparty = td["first_party"];
+	firstparty = addCategoriesToJson(firstparty);
+	
+	for (var key in firstparty) {
+		if (clist[firstparty[key].category] == null)
+			clist[firstparty[key].category] = 1;
+		else
+			clist[firstparty[key].category]++;
+	}
+	
+	// convert into tuples to sort descending by count
+	var tuples = [];
+	for (var key in clist)
+		tuples.push([key, clist[key]]);
+
+	tuples.sort(function(a, b) {
+		a = a[1];
+		b = b[1];
+		return a < b ? 1 : (a > b ? -1 : 0);
+	});
+
+	for (var i = 0; i < tuples.length; i++) {
+		var key = tuples[i][0];
+		var value = tuples[i][1];
+		
+		if (i < 5) { // top N categories display by default
+			active_categories.push({value:key, data:value, enabled:false});
+		} else {
+			other_categories.push({value:key, data:value, enabled:false});
+		}
+	}
+}
+
+function initializeSearchBox() {
+	// set up search box
+	for(var i = 0; i < active_categories.length; i++)
+	{
+		var html = '<div class="active_category_item">' +
+			'<button class="delete_item" onclick="remove_active_item(this);"></button>' +
+			'<span class="item_name">' + active_categories[i].value + '</span> <span class="separator">|</span> ' +
+			'<i><span class="item_percent">' + active_categories[i].data + '</span></i>' +
+			'<input type="checkbox">' +
+			'</div>';
+
+		$('#active_categories').append(html);
+	}
+	
+	// setup autocomplete function pulling from categories[] array
+	$('#autocomplete').autocomplete({
+		minLength: 0,
+		lookup: other_categories,
+		onSelect: function (item) {
+			var html = '<div class="active_category_item">' +
+			'<button class="delete_item" onclick="remove_active_item(this);"></button>' +
+			'<span class="item_name">' + item.value + '</span> <span class="separator">|</span> ' +
+			'<i><span class="item_percent">' + item.data + '</span></i>' +
+			'<input type="checkbox">' +
+		'</div>';
+
+			$('#active_categories').append(html);
+			document.getElementById('autocomplete').value = '';
+
+			for (var i = 0; i < other_categories.length; i++) {
+				if (other_categories[i].value == item.value)
+					other_categories.splice(i, 1);
+			}
+
+			active_categories.push(item);
+			$(document).trigger('click');
+			
+			// update
+			loadData();
+		}
+    });
+}
+
 function getCategoryList(datalist) {
 	cats = [];
 	for (i=0;i<active_categories.length;i++){
@@ -131,6 +187,7 @@ function visualizeit() {
 	firstparty = td["first_party"];
 	thirdparty = td["third_party"];
 	firstparty = addCategoriesToJson(firstparty);
+	//initializeCategories();
 	
 	// todo: control sort type with buttons
 	// sort by category (and by domain within)
@@ -166,7 +223,6 @@ function loadData() {
 			})
 		.on("click", function() {
 				if (selectedBlock != null) {
-					console.log(selectedBlock[0][0].childNodes[2]);
 					selectedBlock[0][0].removeChild(selectedBlock[0][0].childNodes[2]);
 				}
 				
@@ -223,36 +279,6 @@ function loadData() {
 		;
 	*/
 }
-
-$(function () {
-    // setup autocomplete function pulling from categories[] array
-    $('#autocomplete').autocomplete({
-        minLength: 0,
-        lookup: other_categories,
-        onSelect: function (item) {
-            var html = '<div class="active_category_item">' +
-            '<button class="delete_item" onclick="remove_active_item(this);"></button>' +
-            '<span class="item_name">' + item.value + '</span> <span class="separator">|</span> ' +
-            '<i><span class="item_percent">' + item.data + '</span></i>' +
-            '<input type="checkbox">' +
-        '</div>';
-
-            $('#active_categories').append(html);
-            document.getElementById('autocomplete').value = '';
-
-            for (var i = 0; i < other_categories.length; i++) {
-                if (other_categories[i].value == item.value)
-                    other_categories.splice(i, 1);
-            }
-
-            active_categories.push(item);
-            $(document).trigger('click');
-			
-			// update
-			loadData();
-        }
-    });
-});
 
 function pop_to_top(category_item)
 {
